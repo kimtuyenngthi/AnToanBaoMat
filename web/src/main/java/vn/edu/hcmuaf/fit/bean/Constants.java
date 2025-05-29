d8e11c3fade2852f9dc68f3b14775745f1dcce8f
kimtuyenngthi <20130147@st.hcmuaf.edu.vn>
2025-05-29 20:20:45 +0700

    up code

---
+++ web/src/main/java/vn/edu/hcmuaf/fit/bean/Constants.java
@@ -0,0 +1,15 @@
+package vn.edu.hcmuaf.fit.bean;
+
+public class Constants {
+
+    public static String GOOGLE_CLIENT_ID = "";
+
+    public static String GOOGLE_CLIENT_SECRET = "";
+
+    public static String GOOGLE_REDIRECT_URI = "";
+    public static String GOOGLE_LINK_GET_TOKEN = "";
+    public static String GOOGLE_LINK_GET_USER_INFO = "";
+
+    public static String GOOGLE_GRANT_TYPE = "";
+
+}
